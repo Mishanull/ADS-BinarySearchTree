a7b9d11fbd33f73373c35b107193485d6ed3322a
Emi-27 <emijos3@gmail.com>
2022-10-16 22:45:40 +0200

    BinaryTree

--- src/main/java/org/fantasticGroup/BinaryTree.java
+++ src/main/java/org/fantasticGroup/BinaryTree.java
@@ -1,5 +1,7 @@
 package org.fantasticGroup;
 
+import java.util.ArrayList;
+
 public class BinaryTree<E> {
     private BinaryTreeNode<E> root;
     int size;
@@ -21,7 +23,14 @@ public class BinaryTree<E> {
         this.root = root;
     }
 
-    public int getSize(BinaryTreeNode<E> current) {
+    public boolean isEmpty() {
+        return root == null || root.getElement() == null;
+    }
+
+    public int size() {
+        return getSize(root);
+    }
+    private int getSize(BinaryTreeNode<E> current) {
         if(current==null){
             return 0;
         }
@@ -29,7 +38,11 @@ public class BinaryTree<E> {
 
     }
 
-    public boolean contains(BinaryTreeNode<E> current, E e) {
+    public boolean contains(E e) {
+        return contains(root, e);
+    }
+
+    private boolean contains(BinaryTreeNode<E> current, E e) {
         boolean aux = false;
         if(root==null){
             System.out.println("Empty tree");
@@ -46,5 +59,83 @@ public class BinaryTree<E> {
         }
 
         return aux;
+    }
+
+    public ArrayList<E> inOrder() {
+        ArrayList<E> elements = new ArrayList<>();
+        return inOrder(elements, root);
+    }
+
+    private ArrayList<E> inOrder(ArrayList<E> list, BinaryTreeNode<E> current) {
+        if(current == null) {
+            return list;
+        }
+        inOrder(list,current.getLeftChild());
+        list.add(current.getElement());
+        inOrder(list,current.getRightChild());
+        return list;
+    }
+
+    public ArrayList<E> preOrder() {
+        ArrayList<E> elements = new ArrayList<>();
+        return preOrder(elements, root);
+    }
+
+    private ArrayList<E> preOrder(ArrayList<E> list, BinaryTreeNode<E> current) {
+        if(current == null) {
+            return list;
+        }
+        list.add(current.getElement());
+        preOrder(list,current.getLeftChild());
+        preOrder(list,current.getRightChild());
+        return list;
+    }
+
+    public ArrayList<E> postOrder() {
+        ArrayList<E> elements = new ArrayList<>();
+        return postOrder(elements, root);
+    }
+
+    private ArrayList<E> postOrder(ArrayList<E> list, BinaryTreeNode<E> current) {
+        if(current == null) {
+            return list;
+        }
+        postOrder(list,current.getLeftChild());
+        postOrder(list,current.getRightChild());
+        list.add(current.getElement());
+        return list;
+    }
+
+    public ArrayList<E> levelOrder() {
+        ArrayList<E> list = new ArrayList<>();
+        for (int i = 0; i <= height(); i++) {
+            levelOrderAddToList(list, root, i);
+        }
+        return list;
+    }
+
+    private void levelOrderAddToList(ArrayList<E> list, BinaryTreeNode<E> current, int level) {
+        if (current == null) {
+            return;
+        }
+        if (level == 0) {
+            list.add(current.getElement());
+        } else {
+            levelOrderAddToList(list, current.getLeftChild(), level - 1);
+            levelOrderAddToList(list, current.getRightChild(), level - 1);
+        }
+    }
+
+    public int height() {
+        return height(root,0);
+    }
+
+    private int height(BinaryTreeNode<E> current, int count) {
+        if(current == null)
+            return count - 1;
+        int leftHeight = height(current.getLeftChild(), count+1);
+        int rightHeight = height(current.getRightChild(), count+1) ;
+
+        return Math.max(leftHeight, rightHeight);
     }
 }
--- src/test/java/BinaryTreeTests.java
+++ src/test/java/BinaryTreeTests.java
@@ -5,25 +5,79 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.junit.runners.model.TestClass;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @RunWith(JUnit4.class)
 public class BinaryTreeTests {
     BinaryTree binaryTree;
+    BinaryTree<Integer> binaryTreeTest;
     @Before
     public void setUp(){
         binaryTree=new BinaryTree<>();
+        binaryTreeTest = new BinaryTree<>();
+        int a = 25;
+        int b = 15;
+        int c = 50;
+        int d = 10;
+        int e = 22;
+        int f = 35;
+        int g = 70;
+        BinaryTreeNode<Integer> node = new BinaryTreeNode<>();
+        BinaryTreeNode<Integer> node2 = new BinaryTreeNode<>();
+        BinaryTreeNode<Integer> node3 = new BinaryTreeNode<>();
+        BinaryTreeNode<Integer> node4 = new BinaryTreeNode<>();
+        BinaryTreeNode<Integer> node5 = new BinaryTreeNode<>();
+        BinaryTreeNode<Integer> node6 = new BinaryTreeNode<>();
+        BinaryTreeNode<Integer> node7 = new BinaryTreeNode<>();
+        node.setElement(a);
+        node2.setElement(b);
+        node3.setElement(c);
+        node4.setElement(d);
+        node5.setElement(e);
+        node6.setElement(f);
+        node7.setElement(g);
+        node.setLeftChild(node2);
+        node.setRightChild(node3);
+        node2.setLeftChild(node4);
+        node2.setRightChild(node5);
+        node3.setLeftChild(node6);
+        node3.setRightChild(node7);
+        binaryTreeTest.setRoot(node);
     }
     @Test
     public void sizeIfEmpty(){
-        Assert.assertEquals(0, binaryTree.getSize(binaryTree.getRoot()));
+        Assert.assertEquals(0, binaryTree.size());
+        Assert.assertTrue(binaryTree.isEmpty());
     }
     @Test
     public void sizeAfterAddingOneElement(){
         int i=1;
         BinaryTreeNode<Integer> node=new BinaryTreeNode<>(i);
         binaryTree.setRoot(node);
-        Assert.assertEquals(1,binaryTree.getSize(binaryTree.getRoot()));
+        Assert.assertEquals(1,binaryTree.size());
+    }
+
+    @Test
+    public void sizeAfterAddingMultiplesElement(){
+        int a=12;
+        int b=23;
+        int c=76;
+        int d=87;
+        BinaryTreeNode<Integer> node=new BinaryTreeNode<>();
+        BinaryTreeNode<Integer> node2=new BinaryTreeNode<>();
+        BinaryTreeNode<Integer> node3=new BinaryTreeNode<>();
+        BinaryTreeNode<Integer> node4=new BinaryTreeNode<>();
+        node.setElement(a);
+        node2.setElement(b);
+        node3.setElement(c);
+        node4.setElement(d);
+        node.setLeftChild(node2);
+        node.setRightChild(node3);
+        node3.setLeftChild(node4);
+        binaryTree.setRoot(node);
+        Assert.assertEquals(4,binaryTree.size());
     }
     @Test
     public void getRootCorrect(){
@@ -46,6 +100,28 @@ public class BinaryTreeTests {
         node.setElement(b);
         Assert.assertEquals('a', node.getElement().charValue());
     }
+
+    @Test
+    public void getElementInNodeCorrect() {
+        int a = 12;
+        int b = 23;
+        int c = 76;
+        int d = 87;
+        BinaryTreeNode<Integer> node = new BinaryTreeNode<>();
+        BinaryTreeNode<Integer> node2 = new BinaryTreeNode<>();
+        BinaryTreeNode<Integer> node3 = new BinaryTreeNode<>();
+        BinaryTreeNode<Integer> node4 = new BinaryTreeNode<>();
+        node.setElement(a);
+        node2.setElement(b);
+        node3.setElement(c);
+        node4.setElement(d);
+        node.setLeftChild(node2);
+        node.setRightChild(node3);
+        node3.setLeftChild(node4);
+        binaryTree.setRoot(node);
+        Assert.assertEquals(76, node3.getElement(),0);
+    }
+
     @Test
     public void addLeftChildCorrect(){
         double a=12.5;
@@ -56,8 +132,53 @@ public class BinaryTreeTests {
         node2.setElement(b);
         node.setLeftChild(node2);
         Assert.assertEquals(node2, node.getLeftChild());
-        Assert.assertEquals(23.1, node.getLeftChild().getElement(),0.0);
+        Assert.assertEquals(23.1, node.getLeftChild().getElement(),0);
     }
+
+    @Test
+    public void addRightChildCorrect() {
+        int a = 12;
+        int b = 23;
+        int c = 76;
+        int d = 87;
+        BinaryTreeNode<Integer> node = new BinaryTreeNode<>();
+        BinaryTreeNode<Integer> node2 = new BinaryTreeNode<>();
+        BinaryTreeNode<Integer> node3 = new BinaryTreeNode<>();
+        BinaryTreeNode<Integer> node4 = new BinaryTreeNode<>();
+        node.setElement(a);
+        node2.setElement(b);
+        node3.setElement(c);
+        node4.setElement(d);
+        node.setLeftChild(node2);
+        node.setRightChild(node3);
+        node3.setLeftChild(node4);
+        binaryTree.setRoot(node);
+        Assert.assertEquals(node3, node.getRightChild());
+        Assert.assertEquals(76, node.getRightChild().getElement(),0);
+    }
+
+    @Test
+    public void setRootCorrect() {
+        int a = 12;
+        int b = 23;
+        int c = 76;
+        int d = 87;
+        BinaryTreeNode<Integer> node = new BinaryTreeNode<>();
+        BinaryTreeNode<Integer> node2 = new BinaryTreeNode<>();
+        BinaryTreeNode<Integer> node3 = new BinaryTreeNode<>();
+        BinaryTreeNode<Integer> node4 = new BinaryTreeNode<>();
+        node.setElement(a);
+        node2.setElement(b);
+        node3.setElement(c);
+        node4.setElement(d);
+        node.setLeftChild(node2);
+        node.setRightChild(node3);
+        node3.setLeftChild(node4);
+        binaryTree.setRoot(node);
+        Assert.assertEquals(node, binaryTree.getRoot());
+        Assert.assertEquals(12, ((int) binaryTree.getRoot().getElement()),0);
+    }
+
     @Test
     public void containsReturnsTrue(){
         double a=12.5;
@@ -76,7 +197,7 @@ public class BinaryTreeTests {
         node.setRightChild(node3);
         node3.setLeftChild(node4);
         binaryTree.setRoot(node);
-        Assert.assertTrue(binaryTree.contains(binaryTree.getRoot(),d));
+        Assert.assertTrue(binaryTree.contains(d));
     }
     @Test
     public void containsReturnsFalse(){
@@ -99,7 +220,39 @@ public class BinaryTreeTests {
         node.setRightChild(node3);
         node3.setLeftChild(node4);
         binaryTree.setRoot(node);
-        System.out.println(binaryTree.getSize(binaryTree.getRoot()));
-        Assert.assertFalse(binaryTree.contains(binaryTree.getRoot(),e));
+        Assert.assertFalse(binaryTree.contains(e));
+    }
+
+    @Test
+    public void preOrderCorrect(){
+        ArrayList<Integer> inOrder = new ArrayList<>(Arrays.asList(25, 15, 10, 22, 50, 35, 70));
+        ArrayList<Integer> tree = binaryTreeTest.preOrder();
+        Assert.assertEquals(inOrder, tree);
+    }
+
+    @Test
+    public void inOrderCorrect(){
+        ArrayList<Integer> inOrder = new ArrayList<>(Arrays.asList(10, 15, 22, 25, 35, 50, 70));
+        ArrayList<Integer> tree = binaryTreeTest.inOrder();
+        Assert.assertEquals(inOrder, tree);
+    }
+
+    @Test
+    public void postOrderCorrect(){
+        ArrayList<Integer> inOrder = new ArrayList<>(Arrays.asList(10, 22, 15, 35, 70, 50, 25));
+        ArrayList<Integer> tree = binaryTreeTest.postOrder();
+        Assert.assertEquals(inOrder, tree);
+    }
+
+    @Test
+    public void levelOrderCorrect(){
+        ArrayList<Integer> inOrder = new ArrayList<>(Arrays.asList(25, 15, 50, 10, 22, 35, 70));
+        ArrayList<Integer> tree = binaryTreeTest.levelOrder();
+        Assert.assertEquals(inOrder, tree);
+    }
+
+    @Test
+    public void heightCorrect(){
+        Assert.assertEquals(2, binaryTreeTest.height());
     }
 }
